0ddaffd9ea62af1df5e1e0c7dfb078456cc8ccab
CyberZHG <CyberZHG@gmail.com>
2019-05-06 23:01:17 +0800

    Add l1 l2 kernel

---
+++ tests/test_l1_l2_kernel.py
@@ -0,0 +1,47 @@
+import os
+import tempfile
+from unittest import TestCase
+import numpy as np
+from tensorflow.python.keras.layers import Flatten, Dense
+from tensorflow.python.keras.models import Sequential, load_model
+from tf_keras_kervolution_2d import KernelConv2D, L1Kernel, L2Kernel
+
+
+class TestL1L2Kernel(TestCase):
+
+    def test_fit(self):
+        x = np.random.standard_normal((1024, 3, 5, 5))
+        y = (x[:, 1, 2, 3] > 0).astype('int32')
+
+        model = Sequential()
+        model.add(KernelConv2D(
+            input_shape=(3, 5, 5),
+            filters=4,
+            kernel_size=3,
+            kernel_function=L1Kernel(),
+            data_format='channels_first'
+        ))
+        model.add(KernelConv2D(
+            filters=3,
+            kernel_size=3,
+            kernel_function=L2Kernel(),
+            padding='same',
+            data_format='channels_last'
+        ))
+        model.add(Flatten())
+        model.add(Dense(units=2, activation='softmax'))
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+        model.summary()
+
+        model.fit(x, y, epochs=30)
+
+        model_path = os.path.join(tempfile.gettempdir(), 'test_knn_%f.h5' % np.random.random())
+        model.save(model_path)
+        model = load_model(model_path, custom_objects={
+            'KernelConv2D': KernelConv2D,
+            'L1Kernel': L1Kernel,
+            'L2Kernel': L2Kernel,
+        })
+
+        predicted = model.predict(x).argmax(axis=-1)
+        self.assertLess(np.sum(np.abs(y - predicted)), 450)
--- tf_keras_kervolution_2d/kernels.py
+++ tf_keras_kervolution_2d/kernels.py
@@ -2,7 +2,7 @@ from tensorflow.python.keras.layers import Layer
 from tensorflow.python.keras import backend as K
 
 
-__all__ = ['LinearKernel']
+__all__ = ['LinearKernel', 'L1Kernel', 'L2Kernel']
 
 
 class LinearKernel(Layer):
@@ -12,3 +12,23 @@ class LinearKernel(Layer):
 
     def call(self, inputs, **kwargs):
         return K.dot(inputs[0], inputs[1])
+
+
+class L1Kernel(Layer):
+
+    def __init__(self, **kwargs):
+        super(L1Kernel, self).__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        x, kernel = K.expand_dims(inputs[0], axis=-1), inputs[1]
+        return K.mean(K.abs(x - kernel), axis=-2)
+
+
+class L2Kernel(Layer):
+
+    def __init__(self, **kwargs):
+        super(L2Kernel, self).__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        x, kernel = K.expand_dims(inputs[0], axis=-1), inputs[1]
+        return K.mean(K.square(x - kernel), axis=-2)
